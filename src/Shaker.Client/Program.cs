eab24ecbd837af1a2cf7a88fd33197221019fc57
Serhii Dmytryk <dr.voo1646@gmail.com>
2023-08-11 13:27:44 +0300

    update ui style in initial page;

--- src/Shaker.Client/Program.cs
+++ src/Shaker.Client/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddScoped<DataService>();
 builder.Services.AddSingleton(_ => new DataService("DefaultEndpointsProtocol=https;AccountName=myshaker;AccountKey=l4MNuLavCOal6Y5HnkabSYibXo3n6M2GuoWlAJ1CfZrvkL9pF12BAxAiUT7GOl3Vg+y1KAjChyCq+AStLid1UQ==;EndpointSuffix=core.windows.net"));
 
 var host = builder.Build();
+
 var dataService = host.Services.GetRequiredService<DataService>();
 await dataService.SetCocktailsAndIngredientsCache();
 await host.RunAsync();
