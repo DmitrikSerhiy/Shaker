e39465990f0d12b748961df69dfd7010be29dd93
Serhii Dmytryk <dr.voo1646@gmail.com>
2023-08-11 13:59:27 +0300

    add ui to cocktails card

---
+++ src/Shaker.Client/Helpers/SearchHelper.cs
@@ -0,0 +1,15 @@
+﻿using Shaker.Client.Dtos;
+
+namespace Shaker.Client.Helpers; 
+
+public sealed class SearchHelper {
+    public static List<Cocktail> Search(List<Cocktail> cocktails, string? query) {
+        return string.IsNullOrWhiteSpace(query)
+            ? cocktails
+            : cocktails
+                .Where(c =>
+                    c.Name.Contains(query, StringComparison.InvariantCultureIgnoreCase) ||
+                    c.Ingredients.Any(i => i.Name.Contains(query, StringComparison.InvariantCultureIgnoreCase)))
+                .ToList();
+    }
+}
