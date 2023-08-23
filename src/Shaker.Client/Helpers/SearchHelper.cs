0845ce3e46e8e7af6c1f31c7079079eb37b7bdd4
Serhii Dmytryk <dr.voo1646@gmail.com>
2023-08-23 15:55:02 +0300

    add search to ingredients page

--- src/Shaker.Client/Helpers/SearchHelper.cs
+++ src/Shaker.Client/Helpers/SearchHelper.cs
@@ -3,13 +3,22 @@
 namespace Shaker.Client.Helpers; 
 
 public sealed class SearchHelper {
-    public static List<Cocktail> Search(List<Cocktail> cocktails, string? query) {
+    public static List<Cocktail> SearchCocktails(List<Cocktail> cocktails, string? query) {
         return string.IsNullOrWhiteSpace(query)
             ? cocktails
             : cocktails
                 .Where(c =>
                     c.Name.Contains(query, StringComparison.InvariantCultureIgnoreCase) ||
                     c.Ingredients.Any(i => i.Name.Contains(query, StringComparison.InvariantCultureIgnoreCase)))
+                .ToList();
+    }
+    
+    public static List<Ingredient> SearchIngredients(List<Ingredient> ingredients, string? query) {
+        return string.IsNullOrWhiteSpace(query)
+            ? ingredients
+            : ingredients
+                .Where(c =>
+                    c.Name.Contains(query, StringComparison.InvariantCultureIgnoreCase))
                 .ToList();
     }
 }
