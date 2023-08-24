df85802fdfdfb1e5c88046c6762177480c253264
Serhii Dmytryk <dr.voo1646@gmail.com>
2023-08-24 10:05:36 +0300

    add cocktail filters;

--- src/Shaker.Client/Helpers/SearchHelper.cs
+++ src/Shaker.Client/Helpers/SearchHelper.cs
@@ -3,14 +3,42 @@
 namespace Shaker.Client.Helpers; 
 
 public sealed class SearchHelper {
-    public static List<Cocktail> SearchCocktails(List<Cocktail> cocktails, string? query) {
+    
+    
+    public static IQueryable<Cocktail> FilterCocktailsByName(IQueryable<Cocktail> cocktails, string? query) {
         return string.IsNullOrWhiteSpace(query)
-            ? cocktails
+            ? cocktails.AsQueryable()
             : cocktails
                 .Where(c =>
                     c.Name.Contains(query, StringComparison.InvariantCultureIgnoreCase) ||
                     c.Ingredients.Any(i => i.Name.Contains(query, StringComparison.InvariantCultureIgnoreCase)))
-                .ToList();
+                .AsQueryable();
+    }
+    
+    public static IQueryable<Cocktail> FilterCocktailsBySource(IQueryable<Cocktail> cocktails, int source) {
+        cocktails = source switch
+        {
+            1 => cocktails.Where(cocktail => cocktail.IsClassic),
+            2 => cocktails.Where(cocktail => cocktail.IsMine),
+            _ => cocktails
+        };
+
+        return cocktails;
+    }
+    public static IQueryable<Cocktail> FilterCocktailsByType(IQueryable<Cocktail> cocktails, CocktailType type) {
+        cocktails = type switch
+        {
+            CocktailType.LongDrink => cocktails.Where(cocktail => cocktail.Type == CocktailType.LongDrink),
+            CocktailType.Shot => cocktails.Where(cocktail => cocktail.Type == CocktailType.Shot),
+            _ => cocktails
+        };
+
+        return cocktails;
+    }
+
+
+    public static List<Cocktail> SearchCocktails(List<Cocktail> cocktails, string? query) {
+        return FilterCocktailsByName(cocktails.AsQueryable(), query).ToList();
     }
     
     public static List<Ingredient> SearchIngredients(List<Ingredient> ingredients, string? query) {
