b89ad13d0bf192a520c565f382e0d7bdbd9dd76d
Serhii Dmytryk <dr.voo1646@gmail.com>
2023-09-02 14:20:44 +0300

    add cocktails glass icons

---
+++ src/Shaker.Client/Helpers/GlassTypeHelper.cs
@@ -0,0 +1,25 @@
+namespace Shaker.Client.Helpers; 
+
+public static class GlassTypeHelper {
+    public static string GetGlassName(this GlassType type) {
+        return type switch
+        {
+            GlassType.None => "",
+            GlassType.MartiniGlass => "Мартіні",
+            GlassType.MargarithaGlass => "Маргарита",
+            GlassType.WineGlass => "Келих для вина",
+            GlassType.CollinsGlass => "Колінз",
+            GlassType.Mug => "Кухоль",
+            GlassType.NickAndNora => "Нік & Нора",
+            GlassType.OldFashionedGlass => "Олд фешн",
+            GlassType.ShotGlass => "Шот",
+            GlassType.Tumbler => "Тумблер",
+            GlassType.CoffeeGlass => "Айріш кава",
+            GlassType.ChampainGlass => "Флюте",
+            GlassType.SnifterGlass => "Сніфтер",
+            GlassType.HurricaneGlass => "Гарікейн",
+            GlassType.ZombieGlass => "Зомбі",
+            _ => ""
+        };
+    }
+}
