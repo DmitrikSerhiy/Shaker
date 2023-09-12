662785afa93997a992199e3eb6a936a605a06189
Serhii Dmytryk <dr.voo1646@gmail.com>
2023-09-12 12:23:03 +0300

    add cocktails amount to increase

--- src/Shaker.Client/Dtos/Ingredient.cs
+++ src/Shaker.Client/Dtos/Ingredient.cs
@@ -7,6 +7,7 @@ public sealed record Ingredient : IComparable<Ingredient> {
     public int Id { get; set; }
     public string Name { get; set; } = null!;
     public IngredientGroup Group { get; set; }
+    public int CocktailsAmountToIncrease { get; set; }
     public int CompareTo(Ingredient? other) {
         return other == null ? 1 : string.Compare(Name, other.Name, StringComparison.InvariantCultureIgnoreCase);
     }
