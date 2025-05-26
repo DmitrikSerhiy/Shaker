5ffbfa1d21fabc0547e7b2b5426cc6198a18cafc
Serhii Dmytryk <dr.voo1646@gmail.com>
2025-05-26 21:31:58 +0300

    fix toTop button is not set sometimes; remove toTop bottom padding; migrate to .NET8.0;

---
+++ src/Shaker.Client/wwwroot/scripts/toTop.js
@@ -0,0 +1,21 @@
+﻿function toTop() {
+    if (location.pathname === "/allcocktails") {
+        let allCocktailsScrollable = document.getElementById("all-cocktails-content-wrapper");
+        allCocktailsScrollable.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    } else if (location.pathname === "/mycocktails") {
+        let myCocktailsScrollable = document.getElementById("my-cocktails-content-wrapper");
+        myCocktailsScrollable.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    } else if (location.pathname === "/ingredients") {
+        let myIngredientsScrollable = document.getElementById("my-ingredients-content-wrapper");
+        myIngredientsScrollable.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    }
+}
