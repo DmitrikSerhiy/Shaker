cbd99cb51b5a48df3529fbe4df25722189046b92
Serhii Dmytryk <dr.voo1646@gmail.com>
2025-06-01 13:36:07 +0300

    fix theme toggler;

--- src/Shaker.Client/wwwroot/scripts/themeManager.js
+++ src/Shaker.Client/wwwroot/scripts/themeManager.js
@@ -1,31 +1,5 @@
 function setThemeManager() {
-    const setTheme = theme => {
-        if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-            document.documentElement.setAttribute('data-bs-theme', 'dark')
-        } else {
-            document.documentElement.setAttribute('data-bs-theme', theme)
-        }
-    }
-
     setTheme(getPreferredTheme())
-
-    const showActiveTheme = (theme, focus = false) => {
-        const themeSwitcher = document.querySelector('#dark-switch')
-        const themeSwitcherLabel = document.querySelector('#dark-switch-label')
-
-        if (!themeSwitcher) {
-            return
-        }
-
-        const themeSwitcherText = theme === 'dark' ? 'на очі не бачу' : 'ще тверезий';
-        themeSwitcherLabel.textContent = themeSwitcherText;
-        themeSwitcher.setAttribute('aria-label', themeSwitcherText)
-        themeSwitcher.checked = theme === 'dark';
-
-        if (focus) {
-            themeSwitcher.focus()
-        }
-    }
 
     window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
         const storedTheme = getStoredTheme()
@@ -33,24 +7,53 @@ function setThemeManager() {
             setTheme(getPreferredTheme())
         }
     })
-
-    showActiveTheme(getPreferredTheme());
-
-    document.querySelectorAll('#dark-switch')
-        .forEach(toggle => {
-            toggle.addEventListener('change', () => {
-                const theme = toggle.checked ? 'dark' : 'light';
-                setStoredTheme(theme);
-                setTheme(theme);
-                showActiveTheme(theme, true);
-                muteUnmuteImages();
-            });
-        })
 }
 
+function onThemeChanged() {
+    const themeSwitcher = document.querySelector('#dark-switch')
+    const theme = themeSwitcher.checked ? 'dark' : 'light';
+    setStoredTheme(theme);
+    setTheme(theme);
+    showActiveTheme(theme, true);
+    muteUnmuteImages();
+}
+
+function initThemeSwitcher() {
+    showActiveTheme(getPreferredTheme());
+};
+
+
+const setTheme = (theme) => {
+    if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.documentElement.setAttribute('data-bs-theme', 'dark')
+    } else {
+        document.documentElement.setAttribute('data-bs-theme', theme)
+    }
+
+    console.log('theme is set to ' + theme)
+}
 
 const getStoredTheme = () => localStorage.getItem('theme');
 const setStoredTheme = theme => localStorage.setItem('theme', theme);
+
+const showActiveTheme = (theme, focus = false) => {
+    const themeSwitcher = document.querySelector('#dark-switch')
+    const themeSwitcherLabel = document.querySelector('#dark-switch-label')
+
+    if (!themeSwitcher) {
+        return
+    }
+
+    const themeSwitcherText = theme === 'dark' ? 'на очі не бачу' : 'ще тверезий';
+    themeSwitcherLabel.textContent = themeSwitcherText;
+    themeSwitcher.setAttribute('aria-label', themeSwitcherText)
+    themeSwitcher.checked = theme === 'dark';
+
+    if (focus) {
+        themeSwitcher.focus()
+    }
+}
+
 const getPreferredTheme = () => {
     const storedTheme = getStoredTheme()
     if (storedTheme) {
